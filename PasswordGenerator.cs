5d27dd65009974cf320ec27e5fc4e9c63acda8a1
Максим Мазур <1mazurmaxim12@gmail.com>
2023-03-24 16:21:24 +0200

    додав 3 класи

---
+++ PasswordGenerator.cs
@@ -0,0 +1,27 @@
+﻿using HomeWork4;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork4
+{
+    public static class PasswordGenerator
+    {
+        private static char[] allowedSymbols = { 'f', '@', '!', ']', '>', '/', '*', '%' };
+
+        public static string Generate(int length)
+        {
+            var random = new Random();
+            var password = new char[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                password[i] = allowedSymbols[random.Next(allowedSymbols.Length)];
+            }
+
+            return new string(password);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using HomeWork4;
+
+string str = PasswordGenerator.Generate(3);  // перевірка чи генерується пароль
+Console.WriteLine($"Random password is: " + str);
+
+public class DataBaseEngine
+{
+    public void WriteValue<T>(T value, string password)
+    {
+        DataBaseValue<T> baseValue = new DataBaseValue<T>();
+    }
+
+    public class DataBaseValue<T>
+    {
+        public T Value { get; set; }
+        public string TableName { get; set; }
+    }
+}
